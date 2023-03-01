e3d9cfd17f764f42ec19a95154b76e60b6051659
lucekdj <lucekdj@gmail.com>
2023-02-28 21:51:11 -0600

    Feb 28  Shyam first day framework -DatabaseUtils,hooks

---
+++ src/test/java/utilities/DataBaseUtils.java
@@ -0,0 +1,54 @@
+package utilities;
+
+import java.sql.*;
+
+public class DataBaseUtils {   //   Shyam he transfers old DataBaseClass into Utils
+    // localhost:API address where to go to the server
+
+    static String url = ConfigReader.getConfigProperty("db.url");
+    static String username = ConfigReader.getConfigProperty("db.userName");
+    static String password = ConfigReader.getConfigProperty("db.password");
+
+
+    static Connection connection;
+    static Statement statement;
+    static ResultSet result;
+
+
+    public static void initializeDBProperties() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // here we will register our driver for mySQL - google it
+            // now connect driver              // where to connect (url)
+            connection = DriverManager.getConnection(url, username, password);
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static ResultSet executeQuery(String query) {
+        try {
+            result = statement.executeQuery(query); // we can add limit 3 at the end
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void closeDatabaseConnection() {
+        try {
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+}
+
